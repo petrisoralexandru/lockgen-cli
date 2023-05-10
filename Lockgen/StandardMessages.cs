493f277c844209fd7f4f3bdf9dca9ebe6fae1fe1
petrisoralexandru <kwixacademy@gmail.com>
2023-05-10 17:18:37 +0300

    Added a method that inform the user some password guidlines

--- Lockgen/StandardMessages.cs
+++ Lockgen/StandardMessages.cs
@@ -14,4 +14,20 @@ public static class StandardMessages
         Console.WriteLine(
             "Harnessing the power of the command line interface, Lockgen CLI offers a lightweight and lightning-fast alternative to the Lockgen application.");
     }
+
+    /// <summary>
+    ///     The method is responsible for displaying recommendations for a secure password.
+    /// </summary>
+    public static void ShowPasswordGuidelines()
+    {
+        Console.WriteLine(
+            "To create a secure and random password, please specify the desired number of characters for your password.");
+        Console.WriteLine(
+            "To ensure optimal security, we recommend that you use a minimum of 4 characters and a maximum of 40 characters for your password.");
+        Console.WriteLine();
+        Console.WriteLine("Passwords constructed of specific number of character are considered: ");
+        Console.WriteLine("4-6 characters - Weak");
+        Console.WriteLine("7-8 characters - Average");
+        Console.WriteLine("9/9+ characters - Strong");
+    }
 }
