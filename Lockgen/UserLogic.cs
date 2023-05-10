42c4d142eb9caa22c96425a345ff04a63ac56194
petrisoralexandru <kwixacademy@gmail.com>
2023-05-10 21:15:03 +0300

    Added a class method that request from the user the number of characters for the password

--- Lockgen/UserLogic.cs
+++ Lockgen/UserLogic.cs
@@ -5,5 +5,25 @@
 /// </summary>
 public class UserLogic
 {
-    
+    /// <summary>
+    ///     The function is responsible for retrieving the required number of characters to build the password
+    /// </summary>
+    /// <param name="message">The message that will be displayed to the user on the screen asking for the required number of characters of the password</param>
+    /// <returns>Returns the number of characters for the password</returns>
+    public static int GetPasswordSize(string message)
+    {
+        string? output = string.Empty;
+        int size = 0;
+        
+        while (string.IsNullOrWhiteSpace(output))
+        {
+            Console.Write(message);
+            output = Console.ReadLine();
+            int.TryParse(output, out size);
+        }
+        
+        Console.WriteLine($"Your password will be made up of {size} characters");
+
+        return size;
+    }
 }
