5cde72ae135b7dd3dfc30246bb8c73208141c338
petrisoralexandru <kwixacademy@gmail.com>
2023-05-10 21:08:01 +0300

    Added a class that is responsible for requesting data from user to generate a password

---
+++ Lockgen/UserLogic.cs
@@ -0,0 +1,9 @@
+﻿namespace Lockgen;
+
+/// <summary>
+///     Class that is responsible for requesting the information needed to generate a password from a user
+/// </summary>
+public class UserLogic
+{
+    
+}
