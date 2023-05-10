b56922a96d80c751b6e7fe294e4f6185c8821fa1
petrisoralexandru <kwixacademy@gmail.com>
2023-05-10 22:27:27 +0300

    Added a class function that ask the user a password option

--- Lockgen/UserLogic.cs
+++ Lockgen/UserLogic.cs
@@ -3,7 +3,7 @@
 /// <summary>
 ///     Class that is responsible for requesting the information needed to generate a password from a user
 /// </summary>
-public class UserLogic
+public static class UserLogic
 {
     /// <summary>
     ///     The function is responsible for retrieving the required number of characters to build the password
@@ -24,5 +24,37 @@ public class UserLogic
         Console.WriteLine($"Your password will be made up of {size} characters");
 
         return size;
+    }
+
+    /// <summary>
+    ///     The function is responsible for confirming a specific option such as including uppercase/lowercase or other characters for password generation
+    /// </summary>
+    /// <param name="message">The message that will be displayed on the user's screen depending on what options he/she wants for generating the password</param>
+    /// <returns>A binary decision (true/false) about a password option</returns>
+    public static bool ConfirmPasswordWithOption(string message)
+    {
+        string? input = string.Empty;
+        bool output = false;
+        
+        while (input.ToLower() != "y" && input.ToLower() != "n")
+        {
+            Console.Write(message);
+            input = Console.ReadLine();
+
+            if (input.Equals("y"))
+            {
+                output = true;
+            }
+            else if (input.Equals("n"))
+            {
+                output = false;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter 'y' for yes or 'n' for no.");
+            }
+        }
+
+        return output;
     }
 }
