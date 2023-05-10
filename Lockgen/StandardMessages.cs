ddce19e01ce8b00f7d88189cb65f0414ce4f7fa8
petrisoralexandru <kwixacademy@gmail.com>
2023-05-10 17:09:20 +0300

    Added a welcome user method to the StandardMessages.cs

--- Lockgen/StandardMessages.cs
+++ Lockgen/StandardMessages.cs
@@ -5,5 +5,13 @@
 /// </summary>
 public static class StandardMessages
 {
-    
+    /// <summary>
+    ///     The method is responsible for displaying an information and welcome message to the user to the application
+    /// </summary>
+    public static void WelcomeUser()
+    {
+        Console.WriteLine("Welcome to Lockgen CLI, the efficient and streamlined password generator application!");
+        Console.WriteLine(
+            "Harnessing the power of the command line interface, Lockgen CLI offers a lightweight and lightning-fast alternative to the Lockgen application.");
+    }
 }
